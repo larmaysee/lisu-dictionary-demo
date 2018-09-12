ad8536c25f866f37ca82d28fbb19a1c7557e71b8
larmaysee <larmaysee.git@gmail.com>
2018-09-12 11:57:45 +0630

    Update v-0.3.1

--- app.js
+++ app.js
@@ -21,34 +21,6 @@ autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = 'info';
 log.info('App starting...');
 
-
-function sendStatusToWindow(text) {
-	log.info(text);
-	mainWindow.webContents.send('message', text);
-}
-
-autoUpdater.on('checking-for-update', () => {
-	sendStatusToWindow('Checking for update...');
-})
-autoUpdater.on('update-available', (info) => {
-	sendStatusToWindow('Update available.');
-})
-autoUpdater.on('update-not-available', (info) => {
-	sendStatusToWindow('Update not available.');
-})
-autoUpdater.on('error', (err) => {
-	sendStatusToWindow('Error in auto-updater. ' + err);
-})
-autoUpdater.on('download-progress', (progressObj) => {
-	let log_message = "Download speed: " + progressObj.bytesPerSecond;
-	log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-	log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-	sendStatusToWindow(log_message);
-})
-autoUpdater.on('update-downloaded', (info) => {
-	autoUpdater.quitAndInstall();
-});
-
 // initialization and is ready to create browser windows.
 app.on('ready', function () {
 	mainWindow = new BrowserWindow({
@@ -68,3 +40,34 @@ app.on('ready', function () {
 		mainWindow = null;
 	});
 });
+
+function sendStatusToWindow(text) {
+	log.info(text);
+	mainWindow.webContents.send('message', text);
+}
+
+app.on('ready', function () {
+	autoUpdater.checkForUpdates();
+});
+autoUpdater.on('checking-for-update', () => {
+	sendStatusToWindow("Checking for update");
+})
+autoUpdater.on('update-available', (info) => {
+	sendStatusToWindow("Update Available");
+
+})
+autoUpdater.on('update-not-available', (info) => {
+	sendStatusToWindow("Update Not Available");
+
+})
+autoUpdater.on('error', (err) => {
+	sendStatusToWindow("You got error.");
+
+})
+autoUpdater.on('download-progress', (progressObj) => {
+	sendStatusToWindow("You download progress");
+
+})
+autoUpdater.on('update-downloaded', (info) => {
+	autoUpdater.quitAndInstall();
+})
